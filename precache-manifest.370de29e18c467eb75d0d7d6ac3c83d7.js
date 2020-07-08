be550afe3b8f53ea24ffab74df31729595a9e27d
RutujaJagtap7 <rutujajagtap333@gmail.com>
2020-07-08 16:03:19 +0530

    Updates

---
+++ precache-manifest.370de29e18c467eb75d0d7d6ac3c83d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6efdf6f6556e26c4f1de23043e600529",
+    "url": "/React-App/index.html"
+  },
+  {
+    "revision": "386f41fa6e2de11fc455",
+    "url": "/React-App/static/css/main.f618af2e.chunk.css"
+  },
+  {
+    "revision": "e33d29e69669432efcf3",
+    "url": "/React-App/static/js/2.58488f5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-App/static/js/2.58488f5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "386f41fa6e2de11fc455",
+    "url": "/React-App/static/js/main.e9308064.chunk.js"
+  },
+  {
+    "revision": "232676a6b65ed22c1b0f",
+    "url": "/React-App/static/js/runtime-main.999b9e46.js"
+  }
+]);
